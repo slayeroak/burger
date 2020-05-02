cd8adbfd786df6e7f8a84c9ffa448f2ba0ed9a2f
slayeroak <roakley1@villanova.edu>
2020-05-01 23:04:45 -0400

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,14 @@
-var mysql = require("mysql");
-var connection;
+const mysql = require("mysql");
 
-if(process.env.JAWSDB_URL){
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-	connection = mysql.createConnection({
-		port: 3306,
-		host: "localhost",
-		user: "root",
-		password: "password",
-		database: "burgers_db"
-	});
-};
-
-connection.connect(function(err){
-	if(err){
-		console.log("error connecting: " + err.stack);
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
-
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 module.exports = connection;
