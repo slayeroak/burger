3a407ca986e194ce4195fbc80bcb28a2e13ed0c2
slayeroak <roakley1@villanova.edu>
2020-05-01 22:44:18 -0400

    rewrote seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,9 @@
-DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	burger_name VARCHAR(100) not null,
-	devoured BOOLEAN default false,
-	date TIMESTAMP
+CREATE TABLE burgers(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
-  
-INSERT INTO burgers (burger_name) VALUES ("cheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("doublecheeseburger");
-INSERT INTO burgers (burger_name) VALUES ("hamburger");
+USE burgers_db;
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("hamburger",false), ("cheeseburger",false), ("veggieburger",false);
